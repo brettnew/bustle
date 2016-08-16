1bc5ff95d00a6952a01deda353cf784967f67cc3
Brett New and Sam Park <sam@>
2016-08-16 11:30:42 -0700

    Add delete button to admin and remove from user

---
+++ app/routes/admin.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('story');
+  },
+  actions: {
+    save3(params) {
+      var newStory = this.store.createRecord('story', params);
+      newStory.save();
+      this.transitionTo('admin');
+    },
+
+    destroyStory(story) {
+      story.destroyStory();
+      this.transitionTo('admin');
+    }
+  }
+});
